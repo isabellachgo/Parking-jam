892b55383ed2b4d2124837d9ae5ca7e62925cf5e
GALLEGO TRAPERO, SONIA <sonia.gallego@alumnos.upm.es>
2024-05-04 20:36:10 +0200

    correct correct packages

---
+++ src/main/java/es/upm/pproject/parkingjam/parking_jam/controller/controller.java
@@ -0,0 +1,5 @@
+package es.upm.pproject.parkingjam.parking_jam.controller;
+
+public class controller {
+
+}
---
+++ src/main/java/es/upm/pproject/parkingjam/parking_jam/model/Level.java
@@ -0,0 +1,5 @@
+package es.upm.pproject.parkingjam.parking_jam.model;
+
+public class Level {
+
+}

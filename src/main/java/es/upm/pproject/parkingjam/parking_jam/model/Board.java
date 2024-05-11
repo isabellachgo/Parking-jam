9b395e5345896b9b8aa0ce2d800384c2ef36b57d
GALLEGO TRAPERO, SONIA <sonia.gallego@alumnos.upm.es>
2024-05-11 18:54:06 +0200

    Board

--- src/main/java/es/upm/pproject/parkingjam/parking_jam/model/Board.java
+++ src/main/java/es/upm/pproject/parkingjam/parking_jam/model/Board.java
@@ -2,4 +2,22 @@ package es.upm.pproject.parkingjam.parking_jam.model;
 
 public class Board {
 	
+	BoardReader bReader;
+	
+	Integer[][] board;
+	Integer dimensionX;
+	Integer dimensionY;
+	String title;
+	
+	public Board(String filepath) {
+		bReader = new BoardReader(filepath);
+		
+		dimensionX = bReader.getDimensionX();
+		dimensionY = bReader.getDimensionY();
+		title = bReader.getTitle();
+		board = bReader.createBoard();
+		
+	}
+	
+	
 }

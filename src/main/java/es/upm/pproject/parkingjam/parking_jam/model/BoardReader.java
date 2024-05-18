d90d60b615c8c33c6c1b47c4bb9bdd544acd53f8
GALLEGO TRAPERO, SONIA <sonia.gallego@alumnos.upm.es>
2024-05-18 09:19:59 +0200

    BoardReader

--- src/main/java/es/upm/pproject/parkingjam/parking_jam/model/BoardReader.java
+++ src/main/java/es/upm/pproject/parkingjam/parking_jam/model/BoardReader.java
@@ -21,15 +21,15 @@ public class BoardReader {
 	private Pair<Integer,Integer> exit_p;
 	private Map<Character, Vehicle> cars;
 	private Character[][] board;
-	
+
 	String filepathFormat = "src/main/resources/levels/level_%d.txt";
-	
+
 	public BoardReader(int nlevel) throws FileNotFoundException, IOException{
-		
+
 		String filepath = String.format(filepathFormat, nlevel);
 		file = new FileReader(new File(filepath));
 		reader = new BufferedReader(file);
-		
+
 		title = reader.readLine();
 		String dimensions;
 		dimensions = reader.readLine();
@@ -37,37 +37,37 @@ public class BoardReader {
 			dimX = Character.getNumericValue(dimensions.charAt(0));
 			dimY = Character.getNumericValue(dimensions.charAt(2));
 		}
-		
+
 		board = createBoard();
-		
+
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public Integer getDimensionX() {
 		return dimX;
 	}
-	
+
 	public Integer getDimensionY() {
 		return dimY;
 	}
-	
+
 	public Map<Character, Vehicle> getCars(){
 		return cars;
 	}
-	
+
 	public Pair<Integer,Integer> getExit(){
 		return exit_p;
 	}
-	
+
 	public Character[][] getBoard() {
 		return board;
 	}
-	
-	
-	
+
+
+
 	// Reads the file and returns a character array whit the file board if it represents a valid level, otherwise it returns null.
 	// A map is also filled in with the cars on the board
 	private Character[][] createBoard(){
@@ -76,66 +76,66 @@ public class BoardReader {
 		Character c;
 		cars = new HashMap<>();
 		Character[][] board = new Character[dimX][dimY];
-		
+
 		for(int i=0; i<dimY; i++) {
 			try {
 				line=reader.readLine();
-			
+
 				for(int j=0; j<dimX; j++) {
 					c = line.charAt(j);
 					board[j][i] = c;
 					switch(c) {
-						case '+':
-							break;
-							
-						case '@':
-							exit_p = new Pair<Integer,Integer> (j,i);
-							n_exit++;
-							break;
-						
-						case ' ':
-							board[j][i] = null;
-							break;
-							
-						default:
-							if(!cars.containsKey(c)) {
-								Set<Pair<Integer,Integer>> positions = new HashSet<>();
-								Pair<Integer, Integer> p = new Pair<>(j,i);
-								positions.add(p);
-								if(c.equals('*')) {
-									cars.put(c, new Vehicle(c, true, null, positions));
-								}
-								else {
-									cars.put(c, new Vehicle(c, false, null, positions));
-								}
-							} else {
-								Vehicle car = cars.get(c);
-								Set<Pair<Integer,Integer>> positions = car.getPosition();
-								positions.add(new Pair<Integer, Integer>(j,i));
-								car.setPosition(positions);
+					case '+':
+						break;
+
+					case '@':
+						exit_p = new Pair<Integer,Integer> (j,i);
+						n_exit++;
+						break;
+
+					case ' ':
+						board[j][i] = null;
+						break;
+
+					default:
+						if(!cars.containsKey(c)) {
+							Set<Pair<Integer,Integer>> positions = new HashSet<>();
+							Pair<Integer, Integer> p = new Pair<>(j,i);
+							positions.add(p);
+							if(c.equals('*')) {
+								cars.put(c, new Vehicle(c, true, null, positions));
 							}
+							else {
+								cars.put(c, new Vehicle(c, false, null, positions));
+							}
+						} else {
+							Vehicle car = cars.get(c);
+							Set<Pair<Integer,Integer>> positions = car.getPosition();
+							positions.add(new Pair<Integer, Integer>(j,i));
+							car.setPosition(positions);
+						}
 					}
-					
+
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}	
 		}
-		
+
 		// check that there is an exit
 		if(n_exit != 1) return null; //error: no tiene salida o tiene más de una
-		
+
 		// check that there is a redcar
 		if(cars.get('*')==null || cars.get('*').getPosition().size() != 2) return null;
-		
+
 		Iterator<Vehicle> it = cars.values().iterator();
 		while(it.hasNext()) {
 			Vehicle car = (Vehicle) it.next();
-			
+
 			// check car size
 			int length = car.getPosition().size();
 			if(length<2) return null;
-			
+
 			// assign size and direction
 			Iterator<Pair<Integer,Integer>> itv = car.getPosition().iterator();
 			Pair<Integer,Integer> aux1=null;
@@ -151,8 +151,45 @@ public class BoardReader {
 				else car.setDimension(new Pair<>(length, 1));
 			}					
 		}
-		
+
 		return board;
 	}
-		
+
+	// prints the level title, dimensions and board
+	public void printBoard () {
+		System.out.println("title: "+ title);
+		System.out.println("size: "+ dimX+" "+dimY);
+
+		for(int i=0; i<dimY; i++) {
+			for(int j=0; j<dimX; j++) {
+				if(board[j][i]==null) System.out.print(" ");
+				else System.out.print(board[j][i]);
+			}
+			System.out.println("");
+		}
+	}
+
+/*
+	public static void main(String args[]) throws FileNotFoundException, IOException {
+		BoardReader b1 = new BoardReader(1);
+		if(b1.board==null) System.out.println("error al construir tablero");
+		else {
+			b1.printBoard();
+			System.out.println("");
+			System.out.println(b1.getExit().getKey()+" "+b1.getExit().getValue());
+			System.out.println("");
+			Iterator<Vehicle> i = b1.getCars().values().iterator();
+			while(i.hasNext()) {
+				Vehicle v = i.next();
+				System.out.print("["+v.getId()+",("+v.getDimension().getKey()+","+v.getDimension().getValue()+"),");
+				System.out.print("->");
+				Iterator<Pair<Integer,Integer>> p = v.getPosition().iterator();
+				while(p.hasNext()) {
+					Pair<Integer,Integer> pos = p.next();
+					System.out.print("("+pos.getKey()+","+pos.getValue()+") ");
+				}
+			}
+		}
+	}
+*/
 }

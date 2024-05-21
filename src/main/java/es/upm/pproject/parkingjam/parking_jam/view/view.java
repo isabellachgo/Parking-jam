068b53f5e6c1ceebefcfd4f2abb4c6f46832e8af
GALLEGO TRAPERO, SONIA <sonia.gallego@alumnos.upm.es>
2024-05-21 23:48:57 +0200

    commit for pull

--- src/main/java/es/upm/pproject/parkingjam/parking_jam/view/view.java
+++ src/main/java/es/upm/pproject/parkingjam/parking_jam/view/view.java
@@ -1,44 +1,6 @@
 package es.upm.pproject.parkingjam.parking_jam.view;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.FontFormatException;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
-
-import es.upm.pproject.parkingjam.parking_jam.model.Level;
-import es.upm.pproject.parkingjam.parking_jam.model.Vehicle;
-import javafx.util.Pair;
-
-
-public class view extends JFrame {
-
+public class view {
+	
 }
